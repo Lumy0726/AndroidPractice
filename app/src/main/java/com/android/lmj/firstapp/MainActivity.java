5294cbd1af3f95593379730099ecc8df04893990
Lumy0726 <jinny9807@gmail.com>
2017-05-25 00:02:49 +0900

    버튼, Toast메세지 테스트

--- app/src/main/java/com/android/lmj/firstapp/MainActivity.java
+++ app/src/main/java/com/android/lmj/firstapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.android.lmj.firstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onButton1Clicked(View v){
+        Toast.makeText(getApplicationContext(), "잘했어!", Toast.LENGTH_LONG).show();
     }
 }
