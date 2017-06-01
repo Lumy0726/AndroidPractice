3f81de8381852e307e8f08b8ae3d709b52fc08ed
Lumy0726 <jinny9807@gmail.com>
2017-06-02 00:44:43 +0900

    버튼 기능 테스트

--- app/src/main/java/com/android/lmj/firstapp/MainActivity.java
+++ app/src/main/java/com/android/lmj/firstapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.android.lmj.firstapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,5 +22,14 @@ public class MainActivity extends AppCompatActivity {
         //이전 메세지가 사라지기 전까지 새로운 메세지가 뜨지 않는구나..
         Toast.makeText(getApplicationContext(), output, Toast.LENGTH_LONG).show();
         buttonClickNum++;
+    }
+    public void button_GoogleConnect(View v){
+        Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.co.kr"));
+        startActivity(myIntent);
+    }
+
+    public void button_CallConnect(View v){
+        Intent myIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("tel:010-1010-1100"));
+        startActivity(myIntent);
     }
 }
