616288e6fdafee514862c34e6a9a22c57212b1dc
Lumy0726 <jinny9807@gmail.com>
2017-07-01 16:55:51 +0900

    Layout update condition test 버튼은 왜 영어가 항상 대문자일까...

--- app/src/main/java/com/android/lmj/firstapp/SubActivity2.java
+++ app/src/main/java/com/android/lmj/firstapp/SubActivity2.java
@@ -15,13 +15,15 @@ public class SubActivity2 extends AppCompatActivity {
     }
 
     public void onButton6(View v){
+        int bias = Math.round((float)(Math.random() * 100));
         Button button6 = (Button) findViewById(R.id.button6);
-        ConstraintLayout.LayoutParams layout = (ConstraintLayout.LayoutParams)button6.getLayoutParams();
-        ViewGroup.MarginLayoutParams margin = (ViewGroup.MarginLayoutParams)button6.getLayoutParams();
+        ConstraintLayout.LayoutParams button6Layout = (ConstraintLayout.LayoutParams)button6.getLayoutParams();
+        //ViewGroup.MarginLayoutParams margin = (ViewGroup.MarginLayoutParams)button6.getLayoutParams();
         //marginValue=(int)(Math.random() * 200);
         //margin.setMargins(0, marginValue, 0, 0);
-        layout.verticalBias=(float)Math.random();
-        button6.requestLayout();
+        button6Layout.verticalBias=bias / (float)100;
+        button6.setText("움직이는 버튼:" + bias + "%");
+        //button6.requestLayout();
     }
 
     public void onButton7(View v){
