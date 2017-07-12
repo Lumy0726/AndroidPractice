5c483718398b1032b1b8985fc00f0f56cef30ff2
Lumy0726 <jinny9807@gmail.com>
2017-07-12 18:08:06 +0900

    Add image resize button

--- app/src/main/java/com/android/lmj/firstapp/SubActivity3.java
+++ app/src/main/java/com/android/lmj/firstapp/SubActivity3.java
@@ -8,13 +8,15 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SubActivity3 extends AppCompatActivity {
 
     Button button;
     TextView textView;
     ImageView imageView;
-    int imageFlag=1;
+    static int imageFlag=1;
+    static int imageSize=800;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,29 +25,51 @@ public class SubActivity3 extends AppCompatActivity {
         button = (Button)findViewById(R.id.button16);
         textView = (TextView)findViewById(R.id.textView3);
         imageView = (ImageView)findViewById(R.id.imageView);
-        ViewGroup.LayoutParams layoutText = (ViewGroup.LayoutParams) textView.getLayoutParams();
-        ViewGroup.LayoutParams layoutImage = (ViewGroup.LayoutParams) imageView.getLayoutParams();
+        setViewSize(imageSize);
+        setViewFlag(imageFlag);
+        //ViewGroup.LayoutParams layoutText = (ViewGroup.LayoutParams) textView.getLayoutParams();
+        //ViewGroup.LayoutParams layoutImage = (ViewGroup.LayoutParams) imageView.getLayoutParams();
         //layoutText.height = 1500;
         //layoutText.width= 1500;
         //layoutImage.height = 1500;
         //layoutImage.width= 1500;
-        layoutText.height = (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 800, getResources().getDisplayMetrics());
-        layoutImage.height = (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 800, getResources().getDisplayMetrics());
+        //layoutText.height = (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 800, getResources().getDisplayMetrics());
+        //layoutImage.height = (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 800, getResources().getDisplayMetrics());
     }
 
-    public void onButton_Finish(View v){ finish(); }
-    public void onButton_Image(View v){
-        if (imageFlag == 1){
-            imageView.setVisibility(View.INVISIBLE);
-            textView.setVisibility(View.VISIBLE);
-            button.setText("이미지 보이기");
-            imageFlag = 0;
-        }
-        else {
+    protected int dipToPix(int dip){
+        return (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dip, getResources().getDisplayMetrics());
+    }
+    protected void setViewSize(int dip){
+        ViewGroup.LayoutParams layoutText = (ViewGroup.LayoutParams) textView.getLayoutParams();
+        ViewGroup.LayoutParams layoutImage = (ViewGroup.LayoutParams) imageView.getLayoutParams();
+        layoutText.width = layoutText.height = layoutImage.width = layoutImage.height = dipToPix(dip);
+        textView.requestLayout(); imageView.requestLayout();
+    }
+    protected void setViewFlag(int flag){
+        if (flag == 1){
             imageView.setVisibility(View.VISIBLE);
             textView.setVisibility(View.INVISIBLE);
             button.setText("이미지 감추기");
-            imageFlag = 1;
         }
+        else {
+            imageView.setVisibility(View.INVISIBLE);
+            textView.setVisibility(View.VISIBLE);
+            button.setText("이미지 보이기");
+        }
+    }
+    public void onButton_Finish(View v){ finish(); }
+    public void onButton_Image(View v){
+        setViewFlag(imageFlag = (imageFlag + 1) % 2);
+    }
+    public void onButton_ImageEx(View v){
+        setViewSize(imageSize+=100);
+    }
+    public void onButton_ImageCt(View v){
+        if (imageSize < 200){
+            Toast.makeText(getApplicationContext(), "축소 불가", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        setViewSize(imageSize-=100);
     }
 }
