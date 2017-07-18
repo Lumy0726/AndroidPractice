add8111c7ba281d3cd8739e0f85bb20736d158e4
Lumy0726 <jinny9807@gmail.com>
2017-07-18 23:31:14 +0900

    Dialog Theme test Add Timer Reset Button

--- app/src/main/java/com/android/lmj/firstapp/StopActivity.java
+++ app/src/main/java/com/android/lmj/firstapp/StopActivity.java
@@ -2,6 +2,10 @@ package com.android.lmj.firstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class StopActivity extends AppCompatActivity {
 
@@ -10,4 +14,14 @@ public class StopActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_stop);
     }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        TextView textView = (TextView) findViewById(R.id.textView10);
+        Button button = (Button) findViewById(R.id.button25);
+        button.setWidth(textView.getWidth());
+        super.onWindowFocusChanged(hasFocus);
+    }
+
+    public void onButton_Finish(View v){ finish(); }
 }
--- app/src/main/java/com/android/lmj/firstapp/SubActivity5.java
+++ app/src/main/java/com/android/lmj/firstapp/SubActivity5.java
@@ -19,12 +19,12 @@ TimeEvent
 */
 
 public class SubActivity5 extends AppCompatActivity implements TimerAble {
-    static boolean[] timeFlag = new boolean[3];
+    static boolean[] timerFlag = new boolean[3];
     static int[] time = new int[3];
     Timer timer;
     Button[] button = new Button[3];
     static {
-        for (int loop1=0; loop1 < 3; loop1++) timeFlag[loop1] = false;
+        for (int loop1=0; loop1 < 3; loop1++) timerFlag[loop1] = false;
     }
 
     @Override
@@ -36,7 +36,7 @@ public class SubActivity5 extends AppCompatActivity implements TimerAble {
         button[2] = (Button) findViewById(R.id.buttonT2);
         timer = new Timer(this);
         for (int loop1=0; loop1 < 3; loop1++){
-            if (timeFlag[loop1]){
+            if (timerFlag[loop1]){
                 timer.add(loop1, (int)(1000 / Math.pow(2, loop1)));
             }
         }
@@ -72,29 +72,43 @@ public class SubActivity5 extends AppCompatActivity implements TimerAble {
 
     void setButtonText(){
         for (int loop1=0; loop1 < 3; loop1++){
-            button[loop1].setText((time[loop1] / Math.pow(2, loop1)) + "초\n" + ((timeFlag[loop1])?"작동중":"멈춤"));
+            button[loop1].setText((time[loop1] / Math.pow(2, loop1)) + "초\n" + ((timerFlag[loop1])?"작동중":"멈춤"));
         }
     }
     public void onButton_TimeSec0(View v){
-        timeFlag[0] = !timeFlag[0];
-        if (timeFlag[0])timer.add(0, 1000);
+        timerFlag[0] = !timerFlag[0];
+        if (timerFlag[0])timer.add(0, 1000);
         else timer.delete(0);
         setButtonText();
     }
     public void onButton_TimeSec1(View v){
-        timeFlag[1] = !timeFlag[1];
-        if (timeFlag[1])timer.add(1, 500);
+        timerFlag[1] = !timerFlag[1];
+        if (timerFlag[1])timer.add(1, 500);
         else timer.delete(1);
         setButtonText();
         //long test = SystemClock.elapsedRealtime();
         //while (SystemClock.elapsedRealtime() - test < 10000);
     }
     public void onButton_TimeSec2(View v){
-        timeFlag[2] = !timeFlag[2];
-        if (timeFlag[2])timer.add(2, 250);
+        timerFlag[2] = !timerFlag[2];
+        if (timerFlag[2])timer.add(2, 250);
         else timer.delete(2);
         setButtonText();
     }
 
+    public void onButton_TimerReset(View v){
+        for (int loop1=0; loop1 < 3; loop1++){
+            if (timerFlag[loop1]){
+                timer.delete(loop1);
+                timerFlag[loop1]=false;
+            }
+            time[loop1]=0;
+        }
+        setButtonText();
+    }
+    public void onButton_TimerStop(View v){
+        Intent intent = new Intent(getApplicationContext(), StopActivity.class);
+        startActivity(intent);
+    }
     public void onButton_Finish(View v){ finish(); }
 }
